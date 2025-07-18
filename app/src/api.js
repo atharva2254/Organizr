52b287474f05e31a1bfd82080071593ffa1f3766
Atharva Lohar <142513945+atharva2254@users.noreply.github.com>
2025-07-18 19:38:02 +0530

    Update api

--- app/src/api.js
+++ app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API,
+  baseURL: "https://organizr-production.up.railway.app/api",
   withCredentials: true,
 });
 
